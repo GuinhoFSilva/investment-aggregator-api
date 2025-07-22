16e09654860a652508f822bd6a9da432d6937b53
GuinhoFSilva <guilherme.silva@sptech.school>
2025-07-22 11:41:10 -0300

    test: add tests for ValidateEmail and ValidateUsername validation

--- src/test/java/com/guinhofsilva/agregadorinvestimentos/service/UserServiceTest.java
+++ src/test/java/com/guinhofsilva/agregadorinvestimentos/service/UserServiceTest.java
@@ -2,8 +2,10 @@ package com.guinhofsilva.agregadorinvestimentos.service;
 
 import com.guinhofsilva.agregadorinvestimentos.Dto.CreateUserDto;
 import com.guinhofsilva.agregadorinvestimentos.Dto.UpdateUserDto;
+import com.guinhofsilva.agregadorinvestimentos.exceptions.EmailAlreadyUsedException;
 import com.guinhofsilva.agregadorinvestimentos.exceptions.EmptyRequestBodyException;
 import com.guinhofsilva.agregadorinvestimentos.exceptions.ResourceNotFoundException;
+import com.guinhofsilva.agregadorinvestimentos.exceptions.UsernameAlreadyUsedException;
 import com.guinhofsilva.agregadorinvestimentos.model.User;
 import com.guinhofsilva.agregadorinvestimentos.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -40,6 +42,12 @@ class UserServiceTest {
 
     @Captor
     private ArgumentCaptor<UUID> uuidArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> emailArgumentCaptor;
+
+    @Captor
+    private ArgumentCaptor<String> usernameArgumentCaptor;
 
 
     // Todo teste tem que passar por três passos, arrange, act e assert. Cada passo diz o que o teste irá seguir, primeiro ele arruma/organiza tudo necessário, depois chama o trecho que gostaríamos de testar e, por fim, será feita todas as verificações para ver se ele executou tudo necessário.
@@ -131,19 +139,23 @@ class UserServiceTest {
     class UpdateUser {
         @Test
         @DisplayName("Should Update a User With Success")
-        void shouldUpdateAUser() {
-            // Arrange
-            UUID id = UUID.randomUUID();
-            User user = new User(UUID.randomUUID(), "username", "email@email.email", "password", Instant.now(), Instant.now());
-            doReturn(Optional.of(user)).when(userRepository).findById(id);
-            doReturn(user).when(userRepository).save(any(User.class));
-            UpdateUserDto input = new UpdateUserDto("username", "email", "password");
+            void shouldUpdateAUser() {
+                // Arrange
+                UUID id = UUID.randomUUID();
+                User user = new User(id, "username", "email@email.email", "password", Instant.now(), Instant.now());
+                doReturn(Optional.of(user)).when(userRepository).findById(any(UUID.class));
+                doReturn(user).when(userRepository).save(any(User.class));
+                UpdateUserDto input = new UpdateUserDto("username", "email", "password");
 
-            User output = userService.updateUser(id, input);
+                User output = userService.updateUser(id, input);
 
-            assertNotNull(output);
-            verify(userRepository, times(1)).findById(id);
-            verify(userRepository, times(1)).save(user);
+                assertNotNull(output);
+                verify(userRepository, times(1)).findById(uuidArgumentCaptor.capture());
+                verify(userRepository, times(1)).save(userArgumentCaptor.capture());
+                assertEquals(input.username(), userArgumentCaptor.getValue().getUsername());
+                assertEquals(input.email(), userArgumentCaptor.getValue().getEmail());
+                assertEquals(input.password(), userArgumentCaptor.getValue().getPassword());
+                assertEquals(id, uuidArgumentCaptor.getValue());
         }
 
         @Test
@@ -151,18 +163,114 @@ class UserServiceTest {
         void shouldNotUpdateAUserWhenUserNotExists() {
             // Arrange
             UUID id = UUID.randomUUID();
-            doReturn(Optional.empty()).when(userRepository).findById(uuidArgumentCaptor.capture());
+            doReturn(Optional.empty()).when(userRepository).findById(any(UUID.class));
             UpdateUserDto input = new UpdateUserDto("username", "email", "password");
 
 
             assertThrows(ResourceNotFoundException.class, () -> userService.updateUser(id, input));
 
+            verify(userRepository, times(1)).findById(uuidArgumentCaptor.capture());
             assertEquals(id, uuidArgumentCaptor.getValue());
-
-            verify(userRepository, times(1)).findById(uuidArgumentCaptor.getValue());
             verify(userRepository, times(0)).save(any());
         }
     }
+
+    @Nested
+    class ValidateEmail {
+        @Test
+        @DisplayName("Should Validate Email With Success")
+        void shouldValidateEmailWithSuccess() {
+            // Arrange
+            UUID id = UUID.randomUUID();
+            UpdateUserDto input = new UpdateUserDto("username", "email", "password");
+            doReturn(Optional.empty()).when(userRepository).findByEmail(any(String.class));
+            // Act
+            // Assert
+            assertDoesNotThrow(() -> userService.validateEmail(input, id));
+            verify(userRepository, times(1)).findByEmail(emailArgumentCaptor.capture());
+            assertEquals(input.email(), emailArgumentCaptor.getValue());
+        }
+
+        @Test
+        @DisplayName("Should Validate Email If From The Same User")
+        void shouldValidateEmailIfFromTheSameUser() {
+            // Arrange
+            UUID id = UUID.randomUUID();
+            User user = new User(id, "username", "email@email.email", "password", Instant.now(), Instant.now());
+            UpdateUserDto input = new UpdateUserDto("username", "email@email.email", "password");
+
+            doReturn(Optional.of(user)).when(userRepository).findByEmail(any(String.class));
+            // Act
+            // Assert
+            assertDoesNotThrow(() -> userService.validateEmail(input, id));
+            verify(userRepository, times(1)).findByEmail(emailArgumentCaptor.capture());
+            assertEquals(input.email(), emailArgumentCaptor.getValue());
+        }
+
+        @Test
+        @DisplayName("Should Not Validate Email And Throws EmailAlreadyExistsException")
+        void shouldNotValidateEmailThrowsEmailAlreadyExistsException() {
+            // Arrange
+            UUID id = UUID.randomUUID();
+            User user = new User(UUID.randomUUID(), "username", "email@email.email", "password", Instant.now(), Instant.now());
+            UpdateUserDto input = new UpdateUserDto("username", "email@email.email", "password");
+            doReturn(Optional.of(user)).when(userRepository).findByEmail(any(String.class));
+            // Act
+            assertThrows(EmailAlreadyUsedException.class, () -> userService.validateEmail(input, id));
+            // Assert
+            verify(userRepository, times(1)).findByEmail(emailArgumentCaptor.capture());
+            assertEquals(input.email(), emailArgumentCaptor.getValue());
+        }
+    }
+
+    @Nested
+    class ValidateUsername {
+        @Test
+        @DisplayName("Should Validate Username With Success")
+        void shouldValidateUsernameWithSuccess() {
+            // Arrange
+            UUID id = UUID.randomUUID();
+            UpdateUserDto input = new UpdateUserDto("username", "email@email.email", "password");
+            doReturn(Optional.empty()).when(userRepository).findByUsername(any(String.class));
+            // Act
+            // Assert
+            assertDoesNotThrow(() -> userService.validateUsername(input, id));
+            verify(userRepository, times(1)).findByUsername(usernameArgumentCaptor.capture());
+            assertEquals(input.username(), usernameArgumentCaptor.getValue());
+        }
+
+        @Test
+        @DisplayName("Should Validate Username If From The Same User")
+        void shouldValidateUsernameIfFromTheSameUser() {
+            // Arrange
+            UUID id = UUID.randomUUID();
+            User user = new User(id, "username", "email@email.email", "password", Instant.now(), Instant.now());
+            UpdateUserDto input = new UpdateUserDto("username", "email@email.email", "password");
+
+            doReturn(Optional.of(user)).when(userRepository).findByUsername(any(String.class));
+            // Act
+                // Assert
+            assertDoesNotThrow(() -> userService.validateUsername(input, id));
+            verify(userRepository, times(1)).findByUsername(usernameArgumentCaptor.capture());
+            assertEquals(input.username(), usernameArgumentCaptor.getValue());
+        }
+
+        @Test
+        @DisplayName("Should Not Validate Username And Throws UsernameAlreadyExistsException")
+        void shouldNotValidateUsernameThrowsUsernameAlreadyExistsException() {
+            // Arrange
+            UUID id = UUID.randomUUID();
+            User user = new User(UUID.randomUUID(), "username", "email@email.email", "password", Instant.now(), Instant.now());
+            UpdateUserDto input = new UpdateUserDto("username", "email@email.email", "password");
+            doReturn(Optional.of(user)).when(userRepository).findByUsername(any(String.class));
+            // Act
+            assertThrows(UsernameAlreadyUsedException.class, () -> userService.validateUsername(input, id));
+            // Assert
+            verify(userRepository, times(1)).findByUsername(usernameArgumentCaptor.capture());
+            assertEquals(input.username(), usernameArgumentCaptor.getValue());
+        }
+    }
+
 
     @Nested
     class DeleteUser {
